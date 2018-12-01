5a71770228b3b6b52e0b2a2a6ff22d26c6e30918
gabi <gabi.szilvasy@gmail.com>
2018-12-01 13:23:32 +0100

    map page modifications

--- src/ts/map.ts
+++ src/ts/map.ts
@@ -5,18 +5,9 @@ var tl = new TimelineMax();
 var map = document.getElementById('map');
   
  
- tl.to(map, 3, {
-      attr: { viewBox: "335 112 10 10"}
-    });
-  tl.to(map, 8, {delay: 1,
-      attr: { viewBox: "-400 -500 1500 1500"}
-    });
 
- tl.to(map, 3, {delay: 1,
-      attr: { viewBox: "0 0 650.29 329.534"}
-    });
 
-tl.to(map, 3, {delay: 1,
+tl.to(map, 3,{
         attr: { viewBox: "400 100 100 100"}
       });
 tl.to(map, 3, {
@@ -25,8 +16,15 @@ tl.to(map, 3, {
       tl.to(map, 3, {
         attr: { viewBox: "0 0 650.29 329.534"}
       });
-  
+      tl.to(map, 3, {
+        attr: { viewBox: "335 112 10 10"}
+      });
+    tl.to(map, 60, {delay: 1,
+        attr: { viewBox: "-5000 -5000 10500 10500"}
+      });
   
 
-tl.repeat(20).play();
+  
+
+tl.play();
    
